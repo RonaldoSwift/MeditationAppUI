84e1f80cdab9c9416ef4deefcad44b7052f807be
Ronado Vargas <ronaldovh99@gmail.com>
2024-02-06 13:19:15 -0500

    Add SilentMoonView

--- MeditationAppUI/App/MeditationAppUIApp.swift
+++ MeditationAppUI/App/MeditationAppUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MeditationAppUIApp: App {
     var body: some Scene {
         WindowGroup {
-            SignUpAndSignView()
+            SilentMoonView()
         }
     }
 }
---
+++ MeditationAppUI/Presentation/SilentMoon/SilentMoonView.swift
@@ -0,0 +1,75 @@
+//
+//  SilentMoonView.swift
+//  MeditationAppUI
+//
+//  Created by Ronaldo Andre on 5/02/24.
+//
+
+import SwiftUI
+
+struct SilentMoonView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                Image(ImageResource.fondoSilentMoon)
+                    .ignoresSafeArea()
+                
+                VStack(alignment: .center) {
+                    HStack {
+                        Text("S i l e n t")
+                            .font(.custom("HelveticaNeueCyr-Medium", size: 16))
+                        Image(ImageResource.logo)
+                        Text("M o o n")
+                            .font(.custom("HelveticaNeueCyr-Medium", size: 16))
+                    }
+                    .padding(.bottom, 10)
+                    Image(ImageResource.imagenInici)
+                }
+                .padding(.bottom, 10)
+            }
+            
+            Spacer()
+            
+            VStack {
+                Text("We are what we do")
+                    .font(.custom("HelveticaNeueCyr-Bold", size: 30))
+                    .padding(.bottom, 10)
+                
+                VStack {
+                    Text("Thousand of people are usign silent moon")
+                        .padding(.bottom, 5)
+                    Text("for smalls meditation")
+                }
+                .font(.custom("HelveticaNeueCyr-Thin", size: 16))
+                .foregroundStyle(Color.colorLetras)
+                
+                generalButtonComponent(
+                    onClickInSitioWeb: {
+                        print("Click next!!! Sign Up")
+                    },
+                    textoDelButton: "SIGN UP"
+                )
+                
+                HStack {
+                    Text("ALREADY HAVE AN ACCOUNT?")
+                        .font(.custom("HelveticaNeueCyr-Medium", size: 16))
+                        .foregroundStyle(Color.colorLetras)
+                    Button(action: {
+                        print("click Next Log In")
+                    }, label: {
+                        Text("LOG IN")
+                            .font(.custom("HelveticaNeueCyr-Medium", size: 16))
+                            .foregroundColor(Color.colorButton)
+                    })
+                }
+                .padding(.top, 20)
+            }
+            .padding(.bottom, 110)
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    SilentMoonView()
+}
