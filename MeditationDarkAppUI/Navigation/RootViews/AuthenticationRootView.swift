52518703c68b75ea0a41aa4cd796b8723a54f0ca
Carlos Vargas Huaman <carlosleonardovargashuaman@gmail.com>
2024-02-29 13:49:44 -0500

    Added new welcome screen for dark app

---
+++ MeditationDarkAppUI/Navigation/RootViews/AuthenticationRootView.swift
@@ -0,0 +1,19 @@
+//
+//  AuthenticationRootView.swift
+//  MeditationDarkAppUI
+//
+//  Created by Carlos Leonardo Camilo Vargas Huaman on 29/02/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct AuthenticationRootView: View {
+    var body: some View {
+        WelcomeView()
+    }
+}
+
+#Preview {
+    AuthenticationRootView()
+}
---
+++ MeditationDarkAppUI/Navigation/RootViews/PrincipalRootView.swift
@@ -0,0 +1,19 @@
+//
+//  PrincipalRootView.swift
+//  MeditationDarkAppUI
+//
+//  Created by Carlos Leonardo Camilo Vargas Huaman on 29/02/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct PrincipalRootView: View {
+    var body: some View {
+        WelcomeView()
+    }
+}
+
+#Preview {
+    PrincipalRootView()
+}
